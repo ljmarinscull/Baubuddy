6524f6431574104a4a1ffcb10872ca66acce0bb1
Lazaro Jesus Marin Scull <ljmarinscull@gmail.com>
2024-03-14 10:29:42 +0200

    Adding ResourcesDao

---
+++ app/src/main/java/com/ljmarinscull/baubuddy/ResourcesDao.kt
@@ -0,0 +1,49 @@
+package com.ljmarinscull.baubuddy
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.ljmarinscull.baubuddy.data.models.ResourceEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ResourcesDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun save(values: List<ResourceEntity>)
+
+    @Query("SELECT * FROM resources")
+    fun findAll(): Flow<List<ResourceEntity>>
+
+    @Query("""
+        SELECT * FROM resources 
+        WHERE task LIKE '%' || :query || '%' 
+        OR title LIKE '%' || :query || '%' 
+        OR sort LIKE '%' || :query || '%' 
+        OR wageType LIKE '%' || :query || '%'
+        OR colorCode LIKE '%' || :query || '%'
+        OR businessUnit LIKE '%' || :query || '%'
+        OR workingTime LIKE '%' || :query || '%'
+        OR parentTaskID LIKE '%' || :query || '%'
+        OR businessUnitKey LIKE '%' || :query || '%' 
+        OR prePlanningBoardQuickSelect LIKE '%' || :query || '%'
+        OR isAvailableInTimeTrackingKioskMode = :isAvailable
+        """)
+    fun findAll(query: String, isAvailable: Boolean): Flow<List<ResourceEntity>>
+
+    @Query("""
+        SELECT * FROM resources 
+        WHERE task LIKE '%' || :query || '%' 
+        OR title LIKE '%' || :query || '%' 
+        OR sort LIKE '%' || :query || '%' 
+        OR wageType LIKE '%' || :query || '%'
+        OR colorCode LIKE '%' || :query || '%'
+        OR businessUnit LIKE '%' || :query || '%'
+        OR workingTime LIKE '%' || :query || '%'
+        OR parentTaskID LIKE '%' || :query || '%'
+        OR businessUnitKey LIKE '%' || :query || '%' 
+        OR prePlanningBoardQuickSelect LIKE '%' || :query || '%'
+        """)
+    fun findAll(query: String): Flow<List<ResourceEntity>>
+}
