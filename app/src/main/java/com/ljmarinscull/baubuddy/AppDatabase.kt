18c7ffeae68ae0b596542106ec423654ff926163
Lazaro Jesus Marin Scull <ljmarinscull@gmail.com>
2024-03-14 10:30:15 +0200

    Adding Database

---
+++ app/src/main/java/com/ljmarinscull/baubuddy/AppDatabase.kt
@@ -0,0 +1,34 @@
+package com.ljmarinscull.baubuddy
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.ljmarinscull.baubuddy.data.models.ResourceEntity
+
+@Database(
+    entities = [ResourceEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun resourcesDao(): ResourcesDao
+
+    companion object {
+        private const val DATABASE_NAME = "database"
+
+        @Volatile private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                buildDatabase(context).also { instance = it }
+            }
+        }
+
+        private fun buildDatabase(context: Context): AppDatabase {
+            return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
+                .build()
+        }
+    }
+}
