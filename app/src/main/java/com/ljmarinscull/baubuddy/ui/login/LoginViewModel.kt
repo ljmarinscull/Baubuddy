c76c3fe7f829ec1d3ad9a1a164c6935aec60b533
Lazaro Jesus Marin Scull <ljmarinscull@gmail.com>
2024-03-15 22:29:47 +0200

    Refactor LoginViewModel

--- app/src/main/java/com/ljmarinscull/baubuddy/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/ljmarinscull/baubuddy/ui/login/LoginViewModel.kt
@@ -2,8 +2,8 @@ package com.ljmarinscull.baubuddy.ui.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
-import com.ljmarinscull.baubuddy.data.datasource.local.AppPreferences
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.liveData
 import com.ljmarinscull.baubuddy.data.datasource.local.IPreferencesRepository
 import com.ljmarinscull.baubuddy.domain.repository.ILoginRepository
 import com.ljmarinscull.baubuddy.domain.repository.IRepository
@@ -11,28 +11,20 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.plus
 
 data class LoginState(
     val isLoading: Boolean = false,
+    val loginButtonEnable: Boolean = true
 )
-
-sealed class LoginEvent {
-    data class Login(val username: String, val password: String) : LoginEvent()
-}
-
 class LoginViewModel(
     private val repository: ILoginRepository,
     private val preferencesRepository: IPreferencesRepository,
     private val dispatcher: CoroutineDispatcher = Dispatchers.Main
 ) : ViewModel() {
+
 
     private  val errorChannel = Channel<String>()
     val errorFlow = errorChannel.receiveAsFlow()
@@ -40,28 +32,22 @@ class LoginViewModel(
     private val _state = MutableStateFlow(LoginState())
     val state = _state.asStateFlow()
 
-    val userLoggedState: StateFlow<AppPreferences> = preferencesRepository.appPreferencesFlow
-        .stateIn(
-            viewModelScope + dispatcher,
-            SharingStarted.WhileSubscribed(5000L),
-            AppPreferences("")
-        )
+    val userLogged = preferencesRepository.appPreferencesFlow.asLiveData()
 
-    private fun login(username: String, password: String) {
-        viewModelScope.launch(dispatcher) {
-            loading()
-            val result = repository.login(username, password)
-            result.fold(
-                onSuccess = { data ->
-                    loading(false)
-                    preferencesRepository.updateAuthorization(data)
-                },
-                onFailure = { throwable ->
-                    loading(false)
-                    errorChannel.send(throwable.localizedMessage!!)
-                }
-            )
-        }
+    fun login(username: String, password: String) = liveData(dispatcher) {
+        loading()
+        val result = repository.login(username, password)
+        loading(false)
+        result.fold(
+            onSuccess = { data ->
+                preferencesRepository.updateAuthorization(data)
+                emit(true)
+            },
+            onFailure = { throwable ->
+                errorChannel.send(throwable.localizedMessage!!)
+                emit(false)
+            }
+        )
     }
 
     private fun loading(value: Boolean = true) {
@@ -71,15 +57,6 @@ class LoginViewModel(
             )
         }
     }
-
-    fun onEvent(event: LoginEvent) {
-        when (event) {
-            is LoginEvent.Login -> {
-                login(event.username, event.password)
-            }
-        }
-    }
-
 }
 
 class LoginViewModelFactory(
